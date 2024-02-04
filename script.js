05a8ff9d1fbf37ff8f37eb4e1663dbf47036a23a
Ana Grynberg <146101559+AnaGrynberg@users.noreply.github.com>
2024-02-04 18:52:59 -0500

    ,

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function changeBackgroundColorOne() {
+    document.body.style.backgroundColor = "blue";
+    document.body.style.transition = 'background 0.3s ease';
+}
+
+function changeBackgroundColorTwo() {
+    document.body.style.backgroundColor = "red";
+    document.body.style.transition = 'background 0.3s ease';
+}
+
+function changeBackgroundColorThree() {
+    document.body.style.backgroundColor = "green";
+    document.body.style.transition = 'background 0.3s ease';
+}
+
+function resetBackgroundColor() {
+    document.body.style.backgroundColor = "";
+}
