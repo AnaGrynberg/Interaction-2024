4d81eb5115b26bd91430ea2c3b1c7aab055540e4
Ana Grynberg <146101559+AnaGrynberg@users.noreply.github.com>
2024-02-23 10:42:44 -0500

    .

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ function changeBackgroundColorThree() {
     document.body.style.transition = 'background 0.3s ease';
 }
 
+function changeBackgroundColorFour() {
+    document.body.style.backgroundColor = "#77F763";
+    document.body.style.transition = 'background 0.3s ease';
+}
+
 function resetBackgroundColor() {
     document.body.style.backgroundColor = "";
 }
